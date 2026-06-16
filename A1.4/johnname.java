9a24a9882600cc9f0fa2a7a98d67f3ec730a92a8
cdkr <hito95979@gmail.com>
2026-06-16 12:01:32 +0700

    1.4.1

---
+++ A1.4/johnname.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class johnname {
+    public static void main(String[] args) {
+        String[] Fname = {
+            "Alphard","Ampiroy","Arnalio","Adachi","Aquioy","Ankuyw",
+            "Berdoy","Bezuli","Boure","Bafloy","Bicao","Bendale","Brown",
+            "Conroy","Caoman","Cleimain","Charduy","Clark","Cineilou","Cpolui",
+            "Davy","Droi"
+        };
+
+        String[] Lname = {
+            "Alerandoy","Alcetroz","Adams","Asemouily","Arthur","Ankevozich",
+            "Borlak","Bernouli","Bourne","Bufula","Benjamin","Brund","Boujei"
+        };
+    
+        System.out.println(Fname[new Random().nextInt(Fname.length)] + " " + Lname[new Random().nextInt(Lname.length)]);
+    }
+}
